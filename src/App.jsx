7bfb0945bc16d6ce60ea8198ca04a14ca533a4de
Tali <talicabana@gmail.com>
2025-08-23 20:06:43 -0300

    Agregué el componente TeoriaComponente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Bienvenidos from "./components/Bienvenidos"
+import TeoriaComponente from "./components/TeoriaComponente";
+
 function App() {
 
-  return <h1>Hola mundo</h1>
+  return (
+  <main className="container my-5">
+  <h1>Hola mundo</h1>
+  <Bienvenidos></Bienvenidos>
+  {/*<Bienvenidos/>*/}
+  <TeoriaComponente/>
+  </main>);
 }
 
 export default App
---
+++ src/components/Bienvenidos.jsx
@@ -0,0 +1,12 @@
+const Bienvenidos = () => {
+    return (
+        <div>
+            <h2>Información de react</h2> 
+            <p>React es una biblioteca de JavaScript desarrolada por Facebook para construor interfaces de usuario. Se destaca por su enfoque en la reación de aplicaciones web de una sola página SPA y su capacidad para manejar la interfaz de una manera eficiente mediante el uso de un DOM virtual.</p>
+            <br />
+            <p>React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+        </div>
+    );
+};
+
+export default Bienvenidos;
