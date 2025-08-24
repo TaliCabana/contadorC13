81b355e382729c6e87f85dd5ebc585b3cd180eaf
Tali <talicabana@gmail.com>
2025-08-24 01:37:05 -0300

    Actualizamos el componente contador en la funcion sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 
 const Contador = () => {
-    const [contador,setContador] = useState(-100)
+  const [contador, setContador] = useState(-100);
   // let contador = 10;
-  const sumar = () => {
-    setContador(contador + 1);
-    console.log(contador);
-  };
-
+  
   return (
     <section className="text-center d-flex justify-content-center mt-5">
       <div className="card w-50">
         <div className="card-body">
           <h5 className="card-title">Contador</h5>
           <p className="card-text fs-3">{contador}</p>
-          <button className="btn btn-primary" onClick={sumar}>
+          <button className="btn btn-primary" onClick={() => setContador(contador + 1)}>
             ➕
           </button>
         </div>
